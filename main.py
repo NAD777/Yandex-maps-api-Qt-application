63e5eed45adde134761cb9dfe9decd7145f0293b
NAD777 <nehaev07.07.2003@gmail.com>
2020-02-27 21:23:23 +0300

    some problems

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
 
 import math
 
@@ -30,7 +31,7 @@ class Example(QMainWindow):
 
         self.refresh()
 
-        self.find_btn.clicked.connect(self.find_func)
+        self.find_btn.clicked.connect(lambda: self.find_func(self.addres.text()))
         self.reset.clicked.connect(self.reset_func)
         self.post_code.stateChanged.connect(self.post_code_func)
 
@@ -38,16 +39,85 @@ class Example(QMainWindow):
 
         self.post_index = ""
 
+    def mousePressEvent(self, event):
+        dy = 30
+        x_cur = event.pos().x() 
+        y_cur = event.pos().y() 
+        if 0 <= x_cur < 600 and dy <= y_cur < 450 + dy: 
+            if event.buttons() == Qt.LeftButton:
+                x = (x_cur - 300) * 360 / 2 ** (self.z + 8) 
+                y = (y_cur - dy - 225) * 230 / 2 ** (self.z + 8) 
+                self.x = float(self.x) + x 
+                self.y = float(self.y) - y
+                print("!!!!: ", self.x, self.y)
+                self.point_x, self.point_y = self.x, self.y
+                self.get_coords(f"{self.x},{self.y}")
+                self.getImage(self.x, self.y)
+                self.set_image()
+                self.update()
+            elif event.buttons() == Qt.RightButton:
+                x = (x_cur - 300) * 360 / 2 ** (self.z + 8) 
+                y = (y_cur - dy - 225) * 230 / 2 ** (self.z + 8) 
+                new_x, new_y = self.get_nearby(self.x + x, self.y - y)
+                if new_x is not None:
+                    self.x, self.y = new_x, new_y
+                    self.point_x, self.point_y = self.x, self.y
+                    self.get_coords(f"{self.x},{self.y}")
+                    self.getImage(self.x, self.y)
+                    self.set_image()
+                    self.update()
+
+    def get_nearby(self, x, y):
+        print([x, y])
+        search_api_server = "https://search-maps.yandex.ru/v1/"
+        api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+        
+        address_ll = f"{x},{y}"
+        search_params = {
+            "apikey": api_key,
+            "text": "",
+            "lang": "ru_RU",
+            "results": 500,
+            "ll": f"{y},{x}",
+            "type": "biz",
+            "spn": "0.001,0.001"
+        }
+        response = requests.get(search_api_server, params=search_params).json()
+        print(response)
+        for company in response["features"]:
+            x_comp, y_comp = company["geometry"]["coordinates"]
+            if self.lonlat_distance((x, y), (x_comp, y_comp)) <= 50:
+                print("get:", x_comp, y_comp)
+                print(company)
+                return x_comp, y_comp
+        return None, None
+
+    def lonlat_distance(self, a, b):
+        degree_to_meters_factor = 111 * 1000  # 111 километров в метрах
+        a_lon, a_lat = a
+        b_lon, b_lat = b
+
+        # Берем среднюю по широте точку и считаем коэффициент для нее.
+        radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+        lat_lon_factor = math.cos(radians_lattitude)
+
+        # Вычисляем смещения в метрах по вертикали и горизонтали.
+        dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+        dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+        # Вычисляем расстояние между точками.
+        distance = math.sqrt(dx * dx + dy * dy)
+
+        return distance
+
     def post_code_func(self):
-        # self.show_post_code = not self.show_post_code
         if self.post_code.isChecked():
             self.label_addres.setText(self.label_addres.text() + ' ' + self.post_index)
         else:
             self.label_addres.setText(self.label_addres.text()[:-len(self.post_index) - 1])
         self.update()
 
-    def find_func(self):
-        text = self.addres.text()
+    def find_func(self, text):
         self.x, self.y = self.get_coords(text)
         self.point_x, self.point_y = self.x, self.y
         self.refresh()
@@ -75,7 +145,8 @@ class Example(QMainWindow):
         except KeyError:
             self.post_index = ""
         print(self.post_index)
-        self.label_addres.setText(obj["GeoObject"]["metaDataProperty"]["GeocoderMetaData"]["text"] + ' ' + self.post_index)
+        self.label_addres.setText(obj["GeoObject"]["metaDataProperty"]["GeocoderMetaData"]["text"] + ' ')
+        self.post_code_func()
         x, y = obj["GeoObject"]["Point"]["pos"].split()
         return x, y
 
@@ -85,7 +156,6 @@ class Example(QMainWindow):
             "ll": f"{x},{y}",
             "l": self.type_map[self.type_iter],
             "z": f"{self.z}"
-
         }
         if self.point_x is not None:
             params["pt"] = f"{self.point_x},{self.point_y},pm2rdm"
